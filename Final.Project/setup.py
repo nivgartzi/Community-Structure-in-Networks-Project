1e1c261a16b3d09ae83d94e79cd6b1434aab2ced
niv.gartzi <75630716+nivgartzi@users.noreply.github.com>
2024-12-26 03:29:49 +0200

    Update setup.py

--- Final.Project/setup.py
+++ Final.Project/setup.py
@@ -6,4 +6,3 @@ setup(name='mykmeanssp',
       description='mykmeanssp setup.py',
       ext_modules=[Extension('mykmeanssp', sources=['spkmeans.c', 'spkmeansmodule.c'])])
 
-# the extension name is like the name in the signaure: PyInit_XXX(void)   (a function in spkmeansmoudle.c)
